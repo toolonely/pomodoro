7ea6200f828c9205302bb0fd490cb74dcfb35675
Me <me@localhost>
2021-05-27 01:06:45 +0300

    add basic counter for pomodoro session

--- pomodoro.py
+++ pomodoro.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
-"""Pomodoro CLI program"""
+"""Pomodoro CLI program
+
+Usage examples:
+
+$ pomodoro.py  # start a pomodoro timer
+
+"""
+
+import time
+
+
+POMODORO_LENGTH = 25 * 60  # pomodoro length in seconds
 
 
 def main():
     """main"""
-    pass
+    counter = POMODORO_LENGTH
+    while counter > 0:
+        print(counter)
+        time.sleep(1)
+        counter -= 1
 
 
 if __name__ == "__main__":
