25aff36e43c280863a393aa4cf2543ce2067cacf
Me <me@localhost>
2021-05-28 00:21:46 +0300

    refactor function into a class

--- pomodoro.py
+++ pomodoro.py
@@ -14,21 +14,24 @@ import time
 POMODORO_LENGTH = 25 * 60  # pomodoro length in seconds
 
 
-def run_pomodoro():
-    """run a pomodoro session"""
-    print("Pomodoro")
-    counter = POMODORO_LENGTH
-    while counter > 0:
-        minutes = int(counter / 60)
-        seconds = counter - minutes * 60
-        print("   {:02d}:{:02d}".format(minutes, seconds), end="\r")
-        time.sleep(1)
-        counter -= 1
+class Pomodoro:
+    """pomodoro session class"""
+    def run(self):
+        """run()"""
+        print("Pomodoro")
+        counter = POMODORO_LENGTH
+        while counter > 0:
+            minutes = int(counter / 60)
+            seconds = counter - minutes * 60
+            print("   {:02d}:{:02d}".format(minutes, seconds), end="\r")
+            time.sleep(1)
+            counter -= 1
 
 
 def main():
     """main"""
-    run_pomodoro()
+    pomodoro = Pomodoro()
+    pomodoro.run()
 
 
 if __name__ == "__main__":
